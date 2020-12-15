3ec00312bc7931a099a9ff0fe8ed466877d19e37
hanzo <enzodurandcontact@gmail.com>
2020-12-15 19:25:06 +0100

    general update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+from neuralnetwork.Layer import Layer
 from neuralnetwork.NeuralNetwork import NeuralNetwork
 
-NB_LAYER = 1
 NB_INPUTS = 4
 NB_NEURONS = 5
 DATA = [[1, 2, 3, 2.5],
@@ -9,7 +9,9 @@ DATA = [[1, 2, 3, 2.5],
 
 
 if __name__ == '__main__':
-    neural_network = NeuralNetwork(NB_LAYER, NB_INPUTS, NB_NEURONS)
+    neural_network = NeuralNetwork()
+    neural_network.add_layer(Layer(NB_INPUTS, NB_NEURONS))
+    neural_network.add_layer(Layer(NB_INPUTS, NB_NEURONS))
     neural_network.forward_layers(DATA)
     neural_network.print_layers_outputs()
 
--- neuralnetwork/NeuralNetwork.py
+++ neuralnetwork/NeuralNetwork.py
@@ -1,21 +1,18 @@
-import numpy as np
-
-from neuralnetwork.Layer import Layer
-
-
 class NeuralNetwork:
 
-    def __init__(self, nb_layer, nb_inputs, nb_neurons):
-        self.nb_layer = nb_layer
+    def __init__(self):
         self.layers = []
-        for i in range(nb_layer):
-            self.layers.append(Layer(nb_inputs, nb_neurons))
+
+    def add_layer(self, layer):
+        self.layers.append(layer)
 
     def forward_layers(self, inputs):
-        for i in range(self.nb_layer):
+        for i in range(len(self.layers)):
             self.layers[i].forward(inputs)
 
     def print_layers_outputs(self):
-        for i in range(self.nb_layer):
-            print("nb_layer : " + str(self.nb_layer))
+        cpt = 0
+        for i in range(len(self.layers)):
+            print("nb_layer : " + str(cpt+1))
             self.layers[i].print_output()
+            cpt += 1
