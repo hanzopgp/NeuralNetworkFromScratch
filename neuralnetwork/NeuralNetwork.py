47c12ccf75d99771c03485eb0cc862513fa53d94
hanzo <enzodurandcontact@gmail.com>
2020-12-20 16:57:47 +0100

    softmax and layers working

--- neuralnetwork/NeuralNetwork.py
+++ neuralnetwork/NeuralNetwork.py
@@ -11,15 +11,15 @@ class NeuralNetwork:
         self.layers.append(layer)
 
     def forward_layers(self, inputs):
-        for i in range(len(self.layers) - 1):
+        for i in range(len(self.layers)):
             if i == 0:
                 self.layers[i].forward(inputs)
             else:
                 self.layers[i].forward(self.layers[i-1].output)
 
     def print_layers_outputs(self):
-        cpt = 0
+        cpt = 1
         for i in range(len(self.layers)):
-            print("nb_layer : " + str(cpt+1))
+            print("nb_layer : " + str(cpt))
             self.layers[i].print_output()
             cpt += 1
