19b031d881e264d02f17d6d1538f88b34566e948
hanzo <enzodurandcontact@gmail.com>
2020-12-22 20:56:55 +0100

    general update

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from neuralnetwork.NeuralNetwork import NeuralNetwork
 from neuralnetwork.Layer import Layer
 
 if __name__ == '__main__':
-    data_example = DataExample(100, 3)  # 100 points on the graph, 3 colors
+    data_example = DataExample(5, 3)  # 100 points on the graph, 3 colors
     data_example.make_data()
     # data_example.display_data()
 
-    neural_network = NeuralNetwork()
+    neural_network = NeuralNetwork(data_example.x)
     neural_network.add_layer(Layer(2, 3, "ReLU"))
     neural_network.add_layer(Layer(3, 3, "softmax"))
-    neural_network.forward_layers(data_example.x)
-
-    print(data_example.x)
+    neural_network.forward_layers()
     neural_network.print_layers_outputs()
--- neuralnetwork/NeuralNetwork.py
+++ neuralnetwork/NeuralNetwork.py
@@ -1,22 +1,27 @@
 class NeuralNetwork:
 
-    def __init__(self):
+    def __init__(self, inputs):
+        self.inputs = inputs
         self.layers = []
         self.output = []
 
     def add_layer(self, layer):
         self.layers.append(layer)
 
-    def forward_layers(self, inputs):
+    def forward_layers(self):
         for i in range(len(self.layers)):
             if i == 0:
-                self.layers[i].forward(inputs)
+                self.layers[i].forward(self.inputs)
             else:
                 self.layers[i].forward(self.layers[i-1].output)
 
     def print_layers_outputs(self):
+        print("========================== TRAINING DATA ==========================")
+        print(self.inputs)
         cpt = 1
-        for i in range(len(self.layers)):
-            print("nb_layer : " + str(cpt))
+        for i in range(len(self.layers) - 1):
+            print("========================== LAYER NUMBER " + str(cpt) + " ==========================")
             self.layers[i].print_output()
             cpt += 1
+        print("========================== OUTPUT PROBABILITIES ==========================")
+        print(self.layers[-1].print_output())
