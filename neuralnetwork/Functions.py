db5ef3c5e5069ebc7472252caf64af85e3fa25b0
hanzo <enzodurandcontact@gmail.com>
2020-12-22 18:08:55 +0100

    updating Functions.py

--- neuralnetwork/Functions.py
+++ neuralnetwork/Functions.py
@@ -4,18 +4,20 @@ import numpy as np
 class Functions:
 
     @staticmethod
-    def activation_step(x):
+    def activation_step(x):  # outdated most of the time because it makes regression harder
+        res = []
         if x > 0:
-            return 1
+            res.append(1)
         else:
-            return 0
+            res.append(0)
+        return res
 
     @staticmethod
-    def activation_sigmoid(x):
+    def activation_sigmoid(x):  # more challenging to compute
         return 1 / (1 + np.exp(-x))
 
     @staticmethod
-    def activation_ReLU(x):
+    def activation_ReLU(x):  # best one most of the time
         return np.maximum(0, x)
 
     @staticmethod
