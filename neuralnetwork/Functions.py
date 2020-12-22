296de1d279903cdbbbfe02ea4f7c8084636aa265
hanzo <enzodurandcontact@gmail.com>
2020-12-22 17:26:50 +0100

    updating Functions.py

--- neuralnetwork/Functions.py
+++ neuralnetwork/Functions.py
@@ -4,6 +4,17 @@ import numpy as np
 class Functions:
 
     @staticmethod
+    def activation_step(x):
+        if x > 0:
+            return 1
+        else:
+            return 0
+
+    @staticmethod
+    def activation_sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+
+    @staticmethod
     def activation_ReLU(x):
         return np.maximum(0, x)
 
@@ -12,6 +23,3 @@ class Functions:
         exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))  # avoiding negative values et overflow errors
         return exp_values / np.sum(exp_values, axis=1, keepdims=True)  # normalizing values
 
-    @staticmethod
-    def normalization_sigmoid(x):
-        return 1 / (1 + np.exp(-x))
