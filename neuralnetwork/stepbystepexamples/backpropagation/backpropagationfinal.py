e75e1d6dcbeefb5bb824da417fb4c20392460aa8
hanzo <enzodurandcontact@gmail.com>
2021-01-26 23:36:46 +0100

    adding stepbystepexamples backpropagationfinal.py
    Took 3 minutes

---
+++ neuralnetwork/stepbystepexamples/backpropagation/backpropagationfinal.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+if __name__ == '__main__':
+    # Passed in gradient from the next layer
+    # for the purpose of this example we're going to use
+    # an array of an incremental gradient values
+    dvalues = np.array([[1., 1., 1.],
+                        [2., 2., 2.],
+                        [3., 3., 3.]])
+    # We have 3 sets of inputs - samples
+    inputs = np.array([[1, 2, 3, 2.5],
+                       [2., 5., -1., 2],
+                       [-1.5, 2.7, 3.3, -0.8]])
+    # We have 3 sets of weights - one set for each neuron
+    # we have 4 inputs, thus 4 weights
+    # recall that we keep weights transposed
+    weights = np.array([[0.2, 0.8, -0.5, 1],
+                        [0.5, -0.91, 0.26, -0.5],
+                        [-0.26, -0.27, 0.17, 0.87]]).T
+    # One bias for each neuron
+    # biases are the row vector with a shape (1, neurons)
+    biases = np.array([[2, 3, 0.5]])
+    # Forward pass
+    layer_outputs = np.dot(inputs, weights) + biases # Dense layer
+    relu_outputs = np.maximum(0, layer_outputs) # ReLU activation
+    # Let's optimize and test backpropagation here
+    # ReLU activation - simulates derivative with respect to input values
+    # from next layer passed to current layer during backpropagation
+    drelu = relu_outputs.copy()
+    drelu[layer_outputs <= 0] = 0
+    # Dense layer
+    # dinputs - multiply by weights
+    dinputs = np.dot(drelu, weights.T)
+    # dweights - multiply by inputs
+    dweights = np.dot(inputs.T, drelu)
+    # dbiases - sum values, do this over samples (first axis), keepdims
+    # since this by default will produce a plain list -
+    # we explained this in the chapter 4
+    dbiases = np.sum(drelu, axis=0, keepdims=True)
+    # Update parameters
+    weights += -0.001 * dweights
+    biases += -0.001 * dbiases
+    print("weights :")
+    print(weights)
+    print("biases :")
+    print(biases)
+
