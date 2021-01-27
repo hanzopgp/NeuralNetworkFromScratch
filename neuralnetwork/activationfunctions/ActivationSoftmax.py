a7caae3e213d739c8111c88c7673ea4594ba2bad
hanzo <enzodurandcontact@gmail.com>
2021-01-27 17:29:11 +0100

    adding backward pass to categorical crossentropy
    Took 28 minutes

--- neuralnetwork/activationfunctions/ActivationSoftmax.py
+++ neuralnetwork/activationfunctions/ActivationSoftmax.py
@@ -9,3 +9,4 @@ class ActivationSoftmax:
     def forward(self, inputs):  # used in output layers to display probabilities
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))  # avoiding negative values et overflow errors
         self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)  # normalizing values
+
--- neuralnetwork/lossfunctions/LossCategoricalCrossentropy.py
+++ neuralnetwork/lossfunctions/LossCategoricalCrossentropy.py
@@ -5,6 +5,9 @@ from neuralnetwork.lossfunctions.Loss import Loss
 
 class LossCategoricalCrossentropy(Loss):
 
+    def __init__(self):
+        self.dinputs = []
+
     def forward(self, y_pred, y_true):
         samples = len(y_pred)  # Number of sample in a batch
         y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)  # Clipping data to prevent div by 0
@@ -13,3 +16,12 @@ class LossCategoricalCrossentropy(Loss):
         elif len(y_true.shape) == 2:
             correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
         return -np.log(correct_confidences)
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)  # Number of samples
+        labels = len(dvalues[0])  # Number of labels in every sample
+        if len(y_true.shape) == 1:  # If labels are sparse,
+            y_true = np.eye(labels)[y_true]  # turn them into one-hot vector
+        self.dinputs = -y_true / dvalues  # Calculate gradient
+        self.dinputs = self.dinputs / samples  # Normalize gradient
+
