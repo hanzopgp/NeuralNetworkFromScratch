fixed implementation softmax/crossentropy combination
Took 27 minutes

neuralnetwork/core/Layer.py
@@ -43,10 +43,14 @@ class Layer:
             self.output = relu.output
 
     def forward_last_layer_and_calculate_loss(self, inputs, y_true):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.synaptic_weights) + self.biases
+
         softmax_crossentropy = ActivationSoftmaxLossCategoricalCrossentropy()
-        loss = softmax_crossentropy.forward(inputs, y_true)
-        softmax_crossentropy.backward(softmax_crossentropy.output, y_true)
+        loss = softmax_crossentropy.forward(self.output, y_true)
         self.output = softmax_crossentropy.output
+
+        softmax_crossentropy.backward(softmax_crossentropy.output, y_true)
         self.dinputs = softmax_crossentropy.dinputs
         return loss
 

neuralnetwork/main.py
@@ -5,6 +5,7 @@ from neuralnetwork.core.NeuralNetwork import NeuralNetwork
 from neuralnetwork.core.Layer import Layer
 from neuralnetwork import settings
 
+
 if __name__ == '__main__':
 
     # Example data
@@ -14,8 +15,8 @@ if __name__ == '__main__':
 
     # Initializing neural network
     neural_network = NeuralNetwork(data_example.x, data_example.y, settings.LOSS_FUNCTION_TYPE)
-    neural_network.add_layer(Layer(2, 3, settings.ACTIVATION_TYPE_FIRST[0]))  # hidden layer + activation type
-    neural_network.add_layer(Layer(3, 3, settings.ACTIVATION_TYPE_FIRST[1]))  # output layer + activation type
+    neural_network.add_layer(Layer(2, 3, settings.ACTIVATION_TYPES[0]))  # hidden layer + activation type
+    neural_network.add_layer(Layer(3, 3, settings.ACTIVATION_TYPES[1]))  # output layer + activation type
 
     # Optimizing neural network using randomness to lower loss
     lowest_loss = 9999999  # some initial value
@@ -29,6 +30,8 @@ if __name__ == '__main__':
     print("Dense 2:")
     print(best_dense2_weights)
     print(best_dense2_biases)
+
+    # Training
     for iteration in range(100_000):
         # Generate a new set of weights for iteration
         neural_network.layers[0].synaptic_weights += 0.05 * np.random.randn(2, 3)