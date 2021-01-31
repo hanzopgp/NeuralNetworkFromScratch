8d109e1eb5134db6cd196bb2e27098edf0dad82d
hanzo <enzodurandcontact@gmail.com>
2021-01-31 17:01:14 +0100

    backward pass of softmax activation
    Took 3 minutes

--- neuralnetwork/activationfunctions/ActivationSoftmax.py
+++ neuralnetwork/activationfunctions/ActivationSoftmax.py
@@ -5,9 +5,15 @@ class ActivationSoftmax:
 
     def __init__(self):
         self.output = []
+        self.dinputs = np.array([])
 
     def forward(self, inputs):  # used in output layers to display probabilities
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))  # avoiding negative values et overflow errors
         self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)  # normalizing values
 
-    def backward(self):
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
