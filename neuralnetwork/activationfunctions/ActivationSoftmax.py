c24af3cd8424dbe50c8a52efdc18085d04bdd6c0
hanzo <enzodurandcontact@gmail.com>
2021-01-31 17:25:32 +0100

    adding crossentropy+softmax maths
    Took 12 minutes

--- neuralnetwork/activationfunctions/ActivationSoftmax.py
+++ neuralnetwork/activationfunctions/ActivationSoftmax.py
@@ -12,8 +12,8 @@ class ActivationSoftmax:
         self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)  # normalizing values
 
     def backward(self, dvalues):
-        self.dinputs = np.empty_like(dvalues)
-        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
-            single_output = single_output.reshape(-1, 1)
+        self.dinputs = np.empty_like(dvalues)  # init array
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):  # enumerate outputs and gradients
+            single_output = single_output.reshape(-1, 1)  # flatten output array
             jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
